7537adc710ce7bf587629babfe8b9f901ca655a7
carolmedici <carolina.obstetriz@gmail.com>
2023-02-27 23:49:51 -0300

    :bug:  placar score
    Fix scoreboard layout and functionality

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -12,12 +12,15 @@ const player2 = "Player 2";
 var playTime = player1;  //player 1 it's always the first player
 var gameOver = false; 
 var winner = "";
-var res1p = 0
-var resTie = 0
-var res2p = 0
+  
+var res1p = 0;
+var resTie = 0;
+var res2p = 0;
 var res1 = document.getElementById('res1')
 var resT = document.getElementById('resT')
 var res2 = document.getElementById('res2')
+
+var theWinner = document.getElementById('the-winner')
 
 function setColor(themeName) {
     localStorage.setItem('theme', themeName);
@@ -28,12 +31,13 @@ changeTheme()
 function changeTheme() {
    if (localStorage.getItem('theme') === 'second-color'){
        setColor('original-color');
+      
         updatePlayerTurn(); //call the function
-        startSpaces();
+        startSpaces(); 
 
     function updatePlayerTurn(){
     if (gameOver == true){
-        return;
+        return ;
     }  // if the game it's over, nothing else will be done
 
     if (playTime == player1) { 
@@ -45,7 +49,7 @@ function changeTheme() {
         player.setAttribute('src', '/assets/images/o.png')
     } 
 }
-    function startSpaces(){
+function startSpaces(){
     let spaces = document.getElementsByClassName("space");
     for (let i = 0; i < spaces.length; i++){
 
@@ -72,60 +76,62 @@ function changeTheme() {
     }} 
     else {
 
-        setColor('second-color');
-        updatePlayerTurn2()
-        startSpaces2()
+       
 
-        function updatePlayerTurn2(){
-            if (gameOver == true){
-                return;
-            }  // if the game it's over, nothing else will be done
-        
-            if (playTime == player1) { 
-        
-                var player = document.querySelectorAll("div#playerturn img")[0];
-                player.setAttribute("src", "/assets/images/dog.png") //if it's player 1 turn's, var player will search the image of #playerturn  and setAttribute will change "src" to "x image"
-            } else{
-                var player = document.querySelectorAll("div#playerturn img")[0];
-                player.setAttribute('src', '/assets/images/cat.png')
-            } 
-        }
-        function startSpaces2(){
-            let spaces = document.getElementsByClassName("space");
-            for (let i = 0; i < spaces.length; i++){
-        
-            spaces[i].addEventListener("click", function(){
+            setColor('second-color');
+            updatePlayerTurn2()
+            startSpaces2()
+    
+            function updatePlayerTurn2(){
                 if (gameOver == true){
                     return;
-                } // if the game it's over, nothing else will be done
+                }  // if the game it's over, nothing else will be done
+            
+                if (playTime == player1) { 
+            
+                    var player = document.querySelectorAll("div#playerturn img")[0];
+                    player.setAttribute("src", "/assets/images/dog.png") //if it's player 1 turn's, var player will search the image of #playerturn  and setAttribute will change "src" to "x image"
+                } else{
+                    var player = document.querySelectorAll("div#playerturn img")[0];
+                    player.setAttribute('src', '/assets/images/cat.png')
+                } 
+            }
+            function startSpaces2(){
+                let spaces = document.getElementsByClassName("space");
+                for (let i = 0; i < spaces.length; i++){
+            
+                spaces[i].addEventListener("click", function(){
+                    if (gameOver == true){
+                        return;
+                    } // if the game it's over, nothing else will be done
+            
+                    if (this.getElementsByTagName("img").length ==0 ){ //if this space is empty, put an image inside. if it's full, nothing happens
+                        if (playTime == player1){
+                            this.innerHTML = "<img src='/assets/images/dog.png'>";
+                            this.setAttribute("move", player1); 
+                            playTime = player2;
+            
+                        }else{
+                            this.innerHTML = "<img src='/assets/images/cat.png'>";
+                            this.setAttribute("move", player2);
+                            playTime = player1;
+                        }}
+                        updatePlayerTurn2(); //update players turn image
+                        thereIsAWinner(); //verify if there is a winner
         
-                if (this.getElementsByTagName("img").length ==0 ){ //if this space is empty, put an image inside. if it's full, nothing happens
-                    if (playTime == player1){
-                        this.innerHTML = "<img src='/assets/images/dog.png'>";
-                        this.setAttribute("move", player1); 
-                        playTime = player2;
         
-                    }else{
-                        this.innerHTML = "<img src='/assets/images/cat.png'>";
-                        this.setAttribute("move", player2);
-                        playTime = player1;
-                    }}
-                    updatePlayerTurn2(); //update players turn image
-                    thereIsAWinner(); //verify if there is a winner
-    
-    
-                })
-    
+                    })
         
-    }}} 
-    (function () {
-        if (localStorage.getItem('theme') === 'second-color') {
-            setColor('second-color');
-        } else {
-            setColor('original-color');
-        }
-     })();
-        }
+            
+        }}} 
+        (function () {
+            if (localStorage.getItem('theme') === 'second-color') {
+                setColor('second-color');
+            } else {
+                setColor('original-color');
+            }
+         })();
+        
     
 
     async function thereIsAWinner(){
@@ -143,47 +149,42 @@ function changeTheme() {
 
         if(winner == ""){ //is empty because we don't have a winner yet
 
-        if((space1 == space2 && space1 == space3 && space1 != "") || (space1 == space4 && space1 == space7 && space1!="") || (space1 == space5 && space1 == space9 && space1 != "")){
+        if((space1 == player1 && space3 == player1 && space2 == player1) || (space1 == player1  && space4 == player1  && space7 ==player1) || (space1 == player1  && space5 == player1  && space9== player1 ) || (space2==player1 && space5==player1 && space8==player1 ) || (space3==player1 && space6==player1 && space9==player1) || (space7==player1 && space8==player1 && space9==player1 )){
 
-        winner = space1
+        winner = player1;
+        gameOver = true
+        res1.innerHTML = res1p++;
+        theWinner.innerHTML = "Player 1";
+        playTime = player1;
+        
+        
+        }else if((space1 == player2 && space3 == player2 && space2 == player2) || (space1 == player2  && space4 == player2  && space7 ==player2) || (space1 == player2  && space5 == player2  && space9== player2 ) || (space2==player2 && space5==player2 && space8==player2 ) || (space3==player2 && space6==player2 && space9==player2) || (space7==player2 && space8==player2 && space9==player2 )){
 
-        }else if((space5 == space4 && space5 == space6 && space5 != "") || (space5 == space2 && space5 == space8 && space5 != "") || (space5 == space3 && space5 == space7 && space5 != "")){
-
-        winner = space5
-
-        }else if((space9 == space8 && space9 == space7 && space9 != "") || (space9 == space6 && space9 == space3 && space9 != "")){
-
-    winner = space9
+            winner = player2;
+            gameOver = true;
+            res2.innerHTML = res2p++;
+            theWinner.innerHTML = "Player 2";
+            playTime = player1;
+                    
         }else if (space1!="" && space2!="" && space3!="" && space4!="" && space5!="" && space6!="" && space7!="" && space8!="" && space9!=""){
-           
-            
-            
+                     
             gameOver == true;
-            return;
+            theWinner.innerHTML = "It's a tie! Nobody wins :)";
+            resT.innerHTML = resTie++
+           
         }
     }
-
-
-if (winner == player1){
-gameOver == true;
-res1.innerHTML = (res1p + 1)
-
-}
-else if (winner == player2){
-    gameOver == true;
-    res2.innerHTML = (res2p + 1)
-    
-    }else{
-        resT.innerHTML = (resTie + 1)
     }
+     
+    
 }
+
+
+
  
 
+
     
-    
-
-
-
 
 
 
@@ -219,7 +220,7 @@ function changeTheme() {
 
 )()*/
 
-function restart(){
+function playAgain(){
    /* window.location.reload();*/
    space1.innerHTML = '';
    space2.innerHTML = '';
@@ -230,8 +231,24 @@ function restart(){
    space7.innerHTML = '';
    space8.innerHTML = '';
    space9.innerHTML = '';
-
-
-    
+playTime == player1;
+  
    
+    }
+
+    function restart(){
+          window.location.reload();
+          space1.innerHTML = '';
+          space2.innerHTML = '';
+          space3.innerHTML = '';
+          space4.innerHTML = '';
+          space5.innerHTML = '';
+          space6.innerHTML = '';
+          space7.innerHTML = '';
+          space8.innerHTML = '';
+          space9.innerHTML = '';
+          res1.innerHTML = " ";
+          res2.innerHTML = "";
+          resT.innerHTML ="";
+          playTime = player1;
     }
